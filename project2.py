0d1f0c702f1b42d281024598c53218a90b6a8a1b
medro25 <58664810+medro25@users.noreply.github.com>
2020-10-21 18:15:37 +0300

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,59 @@
+
+
+import nltk 
+import numpy as np
+
+
+
+
+
+
+f = open("s24_2002.vrt", "r")
+print(f.read()) 
+#f2 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f3 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f4 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f5 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f6 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f7 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f8 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f9 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f10 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f11 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f12 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f13 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f14 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f15 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f16 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+#f17 = open("s24_2002.vrt", "r")
+#print(f.read()) 
+
+
+#vectorizer = CountVectorizer()
+
+# tokenize and build vocab
+#vectorizer.fit(f)
+# summarize
+#print(vectorizer.vocabulary_)
+# encode document
+#vector = vectorizer.transform(text)
+# summarize encoded vector
+#print(vector.shape)
+#plt.plot([len(vector) for i in range(17)])
+
+#plt.show(()
